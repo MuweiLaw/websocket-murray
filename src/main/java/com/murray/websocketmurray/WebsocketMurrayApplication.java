96e68071978f1604105426cf63ac2cfb8f1c6c47
murray <muweilaw@qq.com>
2020-03-04 12:19:50 +0800

    websocket初次demo

---
+++ src/main/java/com/murray/websocketmurray/WebsocketMurrayApplication.java
@@ -0,0 +1,13 @@
+package com.murray.websocketmurray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketMurrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketMurrayApplication.class, args);
+    }
+
+}
